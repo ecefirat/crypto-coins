24c96cb2a018d451ce4b638a285e29ce95cf009d
ecefirat <ece.firatt@gmail.com>
2021-06-28 15:42:06 +1000

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -41,9 +41,9 @@ import Nav from "./nav.svelte"
 
 <main class="p-8 max-w-7xl mx-auto bg-gray-600 font-mono">
 	<!-- <Nav /> -->
-	<h1 class="text-gray-100 text-4xl text-center mt-8 uppercase">Crypto-Tracker</h1>
-	<p class="text-gray-300 text-sm italic text-center">for the highest 20 cryptocurrency</p>
-	<h3 class="text-gray-200 text-2xl text-center italic my-6">Market Cap Rank, Current Price, 24 Hour % Change and Market Cap Information</h3>
+	<h1 class="text-gray-100 md:text-4xl sm:text-xl text-center mt-8 uppercase">Crypto-Tracker</h1>
+	<p class="text-gray-300 md:text-sm sm:text-xs italic text-center">for the highest 20 cryptocurrency</p>
+	<h3 class="text-gray-200 md:text-2xl sm:text-sm text-center italic my-6">Market Cap Rank, Current Price, 24 Hour % Change and Market Cap Information</h3>
 
 <input
 	type="text"
