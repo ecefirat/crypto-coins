d6ae96d9c3f4440491a672ba888996e155ee1bf0
ecefirat <ece.firatt@gmail.com>
2021-06-29 08:51:58 +1000

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,9 @@ module.exports = {
   purge: {
     content: ["./src/**/*.svelte"],
   },
-  darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       mono: ["Menlo", "monospace"],
-    },
-    minWidth: {
-      0: "0",
-      "1/4": "25%",
-      "1/2": "50%",
-      "3/4": "75%",
-      full: "100%",
-      "3hun": "300px",
-      "275hun": "275px",
-      "250hun": "250px",
     },
   },
   variants: {
